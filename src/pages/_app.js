28a18cf35c656007367c612a5de45f9dc326a379
Josh <jozsef.bone@gmail.com>
2025-09-24 00:20:46 +0200

    Updated favicons

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -24,8 +24,12 @@ export default function App({ Component, pageProps }) {
                   type="text/css"
                   dangerouslySetInnerHTML={{__html: mediaStyles}}
               />
-              <link rel="icon" href="/favicon.ico"/>
               <meta name="viewport" content="width=device-width, initial-scale=1"/>
+              <link rel="icon" href="/favicon/favicon.ico" sizes="any" />
+              <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png" />
+              <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png" />
+              <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png" />
+              <link rel="manifest" href="/favicon/site.webmanifest" />
           </Head>
           <MediaContextProvider disableDynamicMediaQueries>
               <main
